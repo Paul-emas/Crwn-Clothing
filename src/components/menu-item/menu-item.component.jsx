bb586fa6fa962e96e751e08583c03d12796d0334
Paul Emas <pjobiggi@gmail.com>
2021-04-20 05:46:54 +0100

    added folder structure

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MenuItem = () => (
+  <div className="menu-item">
+    <div className="content">
+      <h1>HATS</h1>
+      <span>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
